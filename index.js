7972c66c4172161f2e68ffd633e6b77e0800c656
EstherMartz <esther.martz@creditas.com>
2020-04-15 12:53:42 +0200

    Updated store

--- index.js
+++ index.js
@@ -6,19 +6,219 @@ const app = express()
 const port = 3004
 
 const creditasUrl = 'http://localhost:3003'
-
+const PaymentMethodType = {
+  EMPTY: 'EMPTY',
+  FULL: 'FULL',
+  BALLOON: 'BALLOON',
+  BALLOON_RETURN: 'BALLOON_RETURN',
+}
 const store = {
   '106': { // 11 Pro Prateado 512GB
     id: '6b7c0869-5052-4bf4-92b9-97aac9b216a8',
-    name: 'iPhone 11 Pro Prateado 512GB',
+    name: 'iPhone 11 Pro',
     imageUrl: 'https://creditas.vteximg.com.br/arquivos/ids/155769/image-a87c513a1af942d89834ec11ea6d8021.jpg?v=637091096791000000',
+    displayExtraItem: false,
     price: 1000,
+    variation:     {
+      id: 'bd263b9b-ab44-4149-8ac3-623dcdf18fd4',
+      sku: '111',
+      color: {
+        code: '#FAD7BD',
+        nameLiteral: 'BACKEND.PRODUCT.CONFIGURATION.COLOR.GOLD',
+      },
+      capacity: '512GB',
+      minimumMonthlyPrice: 'R$ 289,90',
+      media: [
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155790/image-64696cfb2f544996a8fbf782036103ad.jpg?v=637091098778230000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155790/image-64696cfb2f544996a8fbf782036103ad.jpg?v=637091098778230000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155789/image-74265a6d5f1b4a27a89ea778cdd0b09f.jpg?v=637091098778100000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155789/image-74265a6d5f1b4a27a89ea778cdd0b09f.jpg?v=637091098778100000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155788/image-f37783e1fe464265a5b1a068cda1c5d9.jpg?v=637091098778000000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155788/image-f37783e1fe464265a5b1a068cda1c5d9.jpg?v=637091098778000000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155787/image-06ba3bd8bf7f4a4d9b002d577ce600c9.jpg?v=637091098777870000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155787/image-06ba3bd8bf7f4a4d9b002d577ce600c9.jpg?v=637091098777870000',
+        },
+        {
+          source: 'https://www.youtube.com/embed/cVEemOmHw9Y',
+          thumbnail: 'http://i3.ytimg.com/vi/cVEemOmHw9Y/hqdefault.jpg',
+        },
+      ],
+      paymentMethods: [
+        {
+          id: '84badd6b-cf1c-4080-b5ca-78d94356aa82',
+          type: PaymentMethodType.FULL,
+          installment: 24,
+          monthlyPrice: 'R$ 374,96',
+          interestPercentage: '1.155187482',
+          totalPrice: 'R$ 8.999,00',
+        },
+        {
+          id: '2aef1bcc-0c33-4ad0-9225-5b5a90c80448',
+          type: PaymentMethodType.BALLOON,
+          installment: 24,
+          balloonPercentage: 30.0,
+          interestPercentage: '1.389913429',
+          monthlyPrice: 'R$ 289,90',
+          totalPrice: 'R$ 9.657,30',
+          balloonPrice: 'R$ 2.699,70',
+        },
+      ],
+      sellerPrice: 'R$ 7.820,13',
+      enabled: true,
+    },
+  },
+  '27': { // 11 Pro Verde meia-noite 64GB
+    id: '0fe022a6-a6a3-4cc9-b2f1-8a8ade6d9366',
+    name: 'iPhone 11 Pro',
+    imageUrl: 'https://creditas.vteximg.com.br/arquivos/ids/155495/image-9efbf1afb11c4583819024ea2a2db464.jpg?v=637091088392730000',
+    displayExtraItem: true,
+    price: 1000,
+    variation:     {
+      id: 'bd263b9b-ab44-4149-8ac3-623dcdf18fd4',
+      sku: '111',
+      color: {
+        code: '#4E5851',
+        nameLiteral: 'BACKEND.PRODUCT.CONFIGURATION.COLOR.MIDNIGHT_GREEN',
+      },
+      capacity: '64GB',
+      minimumMonthlyPrice: 'R$ 229,90',
+      media: [
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155790/image-64696cfb2f544996a8fbf782036103ad.jpg?v=637091098778230000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155790/image-64696cfb2f544996a8fbf782036103ad.jpg?v=637091098778230000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155789/image-74265a6d5f1b4a27a89ea778cdd0b09f.jpg?v=637091098778100000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155789/image-74265a6d5f1b4a27a89ea778cdd0b09f.jpg?v=637091098778100000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155788/image-f37783e1fe464265a5b1a068cda1c5d9.jpg?v=637091098778000000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155788/image-f37783e1fe464265a5b1a068cda1c5d9.jpg?v=637091098778000000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155787/image-06ba3bd8bf7f4a4d9b002d577ce600c9.jpg?v=637091098777870000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155787/image-06ba3bd8bf7f4a4d9b002d577ce600c9.jpg?v=637091098777870000',
+        },
+        {
+          source: 'https://www.youtube.com/embed/cVEemOmHw9Y',
+          thumbnail: 'http://i3.ytimg.com/vi/cVEemOmHw9Y/hqdefault.jpg',
+        },
+      ],
+      paymentMethods: [
+        {
+          id: '84badd6b-cf1c-4080-b5ca-78d94356aa82',
+          type: PaymentMethodType.FULL,
+          installment: 24,
+          monthlyPrice: 'R$ 832,54',
+          interestPercentage: '1.155187482',
+          totalPrice: 'R$ 19.981,13',
+        },
+        {
+          id: '2aef1bcc-0c33-4ad0-9225-5b5a90c80448',
+          type: PaymentMethodType.BALLOON,
+          installment: 24,
+          balloonPercentage: 30.0,
+          interestPercentage: '1.389913429',
+          monthlyPrice: 'R$ 832,54',
+          totalPrice: 'R$ 19.981,13',
+          balloonPrice: 'R$ 8.423,10',
+        },
+      ],
+      sellerPrice: 'R$ 6.082,13',
+      enabled: true,
+    },
   },
   '20': { // 11 Pro Max Verde Meia Noite 256
     id: 23456,
-    name: 'iPhone 11 Pro Max Verde Meia Noite 256GB',
+    displayExtraItem: true,
+    name: 'iPhone 11 Pro Max',
     imageUrl: 'https://creditas.vteximg.com.br/arquivos/ids/155467/image-dfb56aaf1492488b9f4eb868d616f98f.jpg?v=637091086249630000',
-    price: 1500
+    price: 1500,
+    variation:     {
+      id: 'bd263b9b-ab44-4149-8ac3-623dcdf18fd4',
+      sku: '111',
+      color: {
+        code: '#FAD7BD',
+        nameLiteral: 'BACKEND.PRODUCT.CONFIGURATION.COLOR.GOLD',
+      },
+      capacity: '512GB',
+      minimumMonthlyPrice: 'R$ 289,90',
+      media: [
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155790/image-64696cfb2f544996a8fbf782036103ad.jpg?v=637091098778230000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155790/image-64696cfb2f544996a8fbf782036103ad.jpg?v=637091098778230000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155789/image-74265a6d5f1b4a27a89ea778cdd0b09f.jpg?v=637091098778100000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155789/image-74265a6d5f1b4a27a89ea778cdd0b09f.jpg?v=637091098778100000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155788/image-f37783e1fe464265a5b1a068cda1c5d9.jpg?v=637091098778000000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155788/image-f37783e1fe464265a5b1a068cda1c5d9.jpg?v=637091098778000000',
+        },
+        {
+          source:
+            'https://creditas.vteximg.com.br/arquivos/ids/155787/image-06ba3bd8bf7f4a4d9b002d577ce600c9.jpg?v=637091098777870000',
+          thumbnail:
+            'https://creditas.vteximg.com.br/arquivos/ids/155787/image-06ba3bd8bf7f4a4d9b002d577ce600c9.jpg?v=637091098777870000',
+        },
+        {
+          source: 'https://www.youtube.com/embed/cVEemOmHw9Y',
+          thumbnail: 'http://i3.ytimg.com/vi/cVEemOmHw9Y/hqdefault.jpg',
+        },
+      ],
+      paymentMethods: [
+        {
+          id: '84badd6b-cf1c-4080-b5ca-78d94356aa82',
+          type: PaymentMethodType.FULL,
+          installment: 24,
+          monthlyPrice: 'R$ 374,96',
+          interestPercentage: '1.155187482',
+          totalPrice: 'R$ 8.999,00',
+        },
+        {
+          id: '2aef1bcc-0c33-4ad0-9225-5b5a90c80448',
+          type: PaymentMethodType.BALLOON,
+          installment: 24,
+          balloonPercentage: 30.0,
+          interestPercentage: '1.389913429',
+          monthlyPrice: 'R$ 289,90',
+          totalPrice: 'R$ 9.657,30',
+          balloonPrice: 'R$ 2.699,70',
+        },
+      ],
+      sellerPrice: 'R$ 7.820,13',
+      enabled: true,
+    },
   },
 }
 
